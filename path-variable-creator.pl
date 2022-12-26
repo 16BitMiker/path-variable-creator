4dec9fe922e2de204a114127d127a1262929236c
Mike Limberger <oh-hai@miker.media>
2022-12-26 18:05:19 -0500

    first commit

---
+++ path-variable-creator.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use Cwd 	   qw||;
+use Data::Dump qw|dump|;
+
+my $path = q|$PATH="%s$PATH"|;
+my $dirs =  [ ];
+my $loc  = q|.|;
+my $list = q||;
+
+LOOP: 
+{
+	while ( glob($loc.qq|/*|) )
+	{
+		if (-e and -d)
+		{
+			push @{$dirs}, s`^\./``r
+		}
+	}
+	
+	if (@{$dirs})
+	{
+		$loc = pop @{$dirs};
+
+		$list .= Cwd::cwd().qq|/${loc}:|;
+		
+		# using the amazing goto construct ;)
+		goto LOOP;
+	}
+}
+
+printf $path.qq|\n|, $list;
+
